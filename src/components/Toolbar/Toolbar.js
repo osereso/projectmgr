aab258edd958c3dc6ab8dddcf6a63f4946c078cb
mbp <mbp@MBP-15-srs.local>
2019-04-23 15:37:10 +0200

    First commit

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>To get started, edit App.js</Text>
+      </View>
+    );
+  }
+}
+
+
